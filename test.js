ed2d02113b1bbd7f8ab8bf78a32e83aac9092720
wetzernutz <126750514+wetzernutz@users.noreply.github.com>
2024-05-21 15:37:23 +1000

    Added autoplay

---
+++ test.js
@@ -0,0 +1,9 @@
+new Carousel(document.getElementById("myCarousel"), {
+    Autoplay: {
+        timeout: 5000,
+        pauseOnHover: false,
+        showProgress: false,
+    }
+}, {
+    Autoplay
+});
