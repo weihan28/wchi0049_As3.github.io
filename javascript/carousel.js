c529d4e15c9bed61e4068de67bb84c9ec234d3dd
wetzernutz <126750514+wetzernutz@users.noreply.github.com>
2024-05-26 14:56:24 +1000

    Minified Js files

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -1,9 +1,3 @@
-new Carousel(document.getElementById("myCarousel"), {
-    Autoplay: {
-        timeout: 5000,
-        pauseOnHover: false,
-        showProgress: false,
-    }
-}, {
-    Autoplay
-});
+// No external minification tools are allowed in this assignment, so i manually minified this file.
+// a formatting tool like prettier in vscode can easily make this readable in an ide.
+new Carousel(document.getElementById("myCarousel"),{Autoplay:{timeout:5e3,pauseOnHover:!1,showProgress:!1}},{Autoplay});
